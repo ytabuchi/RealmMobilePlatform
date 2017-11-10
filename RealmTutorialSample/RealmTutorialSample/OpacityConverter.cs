a09305027150c918172995f4028756380c3b3846
Yoshito Tabuchi @ MBP-Xamarin <ytabuchi.xlsoft@gmail.com>
2017-11-10 18:25:28 +0900

    update xamarin.forms to latest and copy some files from old sample

---
+++ RealmTutorialSample/RealmTutorialSample/OpacityConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace RealmMobilePlatformSample
+{
+    public class OpacityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return 1;
+
+            return (bool)value ? 0.5 : 1;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
