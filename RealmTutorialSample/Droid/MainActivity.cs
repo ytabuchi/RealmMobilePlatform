684ff4fb05eb81293a165cab80a382d0369434e8
ytabuchi @ Surface <ytabuchi.xlsoft@gmail.com>
2017-11-15 13:27:04 +0900

    Update android icons

--- RealmTutorialSample/Droid/MainActivity.cs
+++ RealmTutorialSample/Droid/MainActivity.cs
@@ -10,7 +10,7 @@ using Android.OS;
 
 namespace RealmTutorialSample.Droid
 {
-    [Activity(Label = "RealmTutorialSample.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "RealmTutorialSample", Icon = "@drawable/ic_launcher", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
